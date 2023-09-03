e1f3b9fc52cf51d26a389e519bd802c3a4bf16a7
Salih_dev <111523623+salih90s@users.noreply.github.com>
2023-09-03 16:16:57 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,44 @@
+void main(){
+//test
+  String musteriAdi = "osman";
+  double musteriMoney = 3000 ;
+
+ musteriMoney = musteriMoney / 20;
+
+print('$musteriAdi' " " +   '$musteriMoney');
+
+// test 2
+
+ String bankName = "SA Bank";
+
+ final String userName1 = "fatik";
+
+double userName1money = 100.00;
+
+ final String userName2 = "memet";
+
+ double userName2money = 50.00;
+
+// test 3
+
+
+const int halisahaTotal = 100;
+
+const int musteri1 = 20;
+
+const int musteri2 = 50;
+
+int varolanKapasite = musteri1 + musteri2;
+
+int kalanKapasite = halisahaTotal - varolanKapasite;
+
+
+print ( " Kalan kapasite :  $kalanKapasite ");
+ 
+
+
+
+
+
+
+}
